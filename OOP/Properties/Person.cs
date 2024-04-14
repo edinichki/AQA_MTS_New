11b168ad288ee98fcf984d81d9e852f3113d1c68
edinichki <edinichki@gmail.com>
2024-04-14 23:39:23 +0500

    finita

---
+++ OOP/Properties/Person.cs
@@ -0,0 +1,97 @@
+﻿namespace Properties;
+
+public class Person
+{
+    private string name;
+    private int age;
+    private string surname;
+    private string address;
+
+    public Person()                       //конструктор
+    {
+        surname = "Trostyanko";
+        Address = "Gdansk";
+    }
+
+    // Стандартный вариант работы с блоком данных
+    public void SetName(string name)
+    {
+        this.name = name;
+    }
+
+    public string GetName()
+    {
+        return name;
+    }
+
+    // В C# есть Свойство для доступа к полю name
+    public string Name                                        //это свойство, тк у него нет () и есть два/один из блока - get и set:
+    {
+        get
+        {
+            return name;
+        }
+        set
+        {
+            // Логика проверки при установке значения
+            if (!string.IsNullOrEmpty(value))
+            {
+                name = value;                            //value - единичный параметр, который передается для свойства Name
+            }
+        }
+    }
+
+    // свойство только для записи
+    public int Age
+    {
+        set { age = value; }
+    }
+
+    // свойство только для чтения
+    public string Surname
+    {
+        get { return surname; }            //захардкоженое значение в конструкторе
+    }
+
+    // Вычисляемые свойства
+    public string Fullname
+    {
+        get { return $"{name} {surname}"; }
+    }
+
+    // Модификаторы доступа
+    public string Address
+    {
+        get { return address; }
+
+        protected set { address = value; }
+    }
+
+    // Автоматические свойства
+    //public string Salary { get; set; }        //может и задать, и вернуть значение
+    //public int Salary { get; set; } = 12500;  //или задать значение сразу, не в конструкторе
+    public int Salary { get; private set; }
+
+    // Блок init
+    // Первый способ
+    public string ID { get; init; } = "XXX-712";
+
+    // Второй способ
+    //public Person(string id) => ID = id;
+    // Или так
+    /*
+    public Person(string id)
+    {
+        ID = id;
+    }
+    */
+
+    // Модификатор required
+    /*
+    public required string Passport { get; set; } // - Ошибка при вызове базового конструктора
+    public Person(string passport)
+    {
+        Passport = passport;
+    }
+    */
+}
---
+++ OOP/Properties/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Properties;
+
+Person person = new Person();
+person.SetName("Alex");
+Console.WriteLine($"{person.GetName()}");
+
+person.Name = "Alex1";              //вот тут как раз свойство объекта и мы его можем использовать как переменную
+person.Age = 45;
+//person.Surname = "Trostyanko";  - Ошибка, тк значение не установить - оно уже есть захардкоженое в контрукторе
+//person.Address = "asdasd";  - Ошибка
+
+Console.WriteLine($"{person.Name}");
+// Console.WriteLine($"{person.Age}"); - Ошибка тк есть только свойство set и нет get
+Console.WriteLine($"{person.Surname}");
+Console.WriteLine($"{person.Fullname}");
+Console.WriteLine($"{person.Address}");
+Console.WriteLine($"{person.Salary}");
+
+// Блок init
+// Первый способ
+//person.ID = "YYY-721"; - Ошибка
+Console.WriteLine(person.ID);
+
+// Второй способ
+/*
+Person person1 = new Person("YYY-721");
+Console.WriteLine(person1.ID);
+
+Person person2 = new Person() { ID = "CCC-123" };
+Console.WriteLine(person2.ID);
+*/
+
+//Person person3 = new Person() { Passport = "MC1234567"};
+//Person person4 = new Person("MC1234567"); // Ошибка
