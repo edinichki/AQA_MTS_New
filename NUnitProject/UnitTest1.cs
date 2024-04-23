2eb98fa17d2fd08f94ada91b6f48fb511854d2dc
edinichki <edinichki@gmail.com>
2024-04-24 00:23:48 +0500

    start

--- NUnitProject/UnitTest1.cs
+++ NUnitProject/UnitTest1.cs
@@ -2,11 +2,6 @@ namespace NUnitProject
 {
     public class Tests
     {
-        [SetUp]
-        public void Setup()
-        {
-        }
-
         [Test]
         public void Test1()
         {
